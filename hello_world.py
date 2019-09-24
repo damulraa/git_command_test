a0b125ae40aeb18b3ae56ac453934af6684ef52a
thegreatestlater <thegreatestlater@gmail.com>
2019-09-24 17:24:50 +0900

    make branch BRN-002(finally)

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,5 @@
 print("hello world")
 print("[Damulrra]: pull from remote source modified by [thegreatestlater].")
 print("[Damulrra]: make branch of BRN-001")
+print("[thegreatestlater]: make branch of BRN-002.")
+print("[thegreatestlater]: bugfix...")
